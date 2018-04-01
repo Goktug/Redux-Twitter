280ea327ffc2c753b2bb536fba9f85d2f4afaf95
Goktug Gumus <gktggumus@gmail.com>
2018-04-01 16:06:29 +0300

    tweet model updated

--- Redux-Twitter/Models/Tweet.swift
+++ Redux-Twitter/Models/Tweet.swift
@@ -10,19 +10,31 @@ import Foundation
 import ObjectMapper
 
 class Tweet: Mappable {
+  var id: String!
   var username: String!
   var thumbnailUrl: String!
-  var createdAt: String!
+  var createdAt: Date!
   var text: String!
   var tweetImageUrl: String?
+  
+  var readableCreatedAt: String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .medium
+    dateFormatter.locale = Locale.init(identifier: "tr_TR")
+    
+    return dateFormatter.string(from: createdAt)
+  }
   
   required init?(map: Map) {
     
   }
   
   func mapping(map: Map) {
+    id <- map["id_str"]
     username <- map["user.screen_name"]
     text <- map["text"]
-    thumbnailUrl <- map["user.profile_image_url"]
+    thumbnailUrl <- map["user.profile_image_url_https"]
+    createdAt <- (map["created_at"], CustomDateFormatTransform(formatString: "EEE MMM dd HH:mm:ss Z yyyy"))
+    tweetImageUrl <- map["entities.media.0.media_url_https"]
   }
 }
